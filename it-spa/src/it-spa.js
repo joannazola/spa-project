bac1888add1a3930c4d9f3604e6fe528520dbe97
Joanna Zoła <joannazola97@gmail.com>
2022-04-30 17:39:46 +0200

    hero section with main page

--- it-spa/src/it-spa.js
+++ it-spa/src/it-spa.js
@@ -1,4 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './it-spa.scss';
+import "bootstrap/dist/css/bootstrap.css";
+import "./it-spa.scss";
 
-const main = document.querySelector('main');
+import { Hero } from "./views/Hero";
+import { MainMenu } from "./views/MainMenu";
+
+const main = document.querySelector("main");
+
+main.before(Hero());
+main.append(MainMenu());
+
+document.body.addEventListener("navigate", (event) => {
+  const component = event.detail;
+
+  main.innerHTML = "";
+  main.append(component());
+});
